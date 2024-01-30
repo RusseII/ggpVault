1fadd0c35f40a37d08a13a90bf5e8a1201097c02
bookland <rratcliffe57@gmail.com>
2024-01-30 13:11:36 +0900

    update gitignore

--- src/ggpVault.sol
+++ src/ggpVault.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
+
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
 contract ggpVault is ERC4626 {
     constructor() ERC4626("ggpVault", "xGGP") {
-        _mint(msg.sender, 100)
 }
