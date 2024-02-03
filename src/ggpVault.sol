48b49ef518fdbce1142c2451f11ee48718c0b938
bookland <rratcliffe57@gmail.com>
2024-02-04 07:50:08 +0900

    misc changes

--- src/ggpVault.sol
+++ src/ggpVault.sol
@@ -6,11 +6,19 @@ import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC4626Mock is Ownable, ERC4626 {
+contract ERC4626Mock is Ownable, ERC4626, Initializable, BaseUpgradeable {
     using SafeERC20 for IERC20;
     uint256 public stakingTotalAssets;
     event WithdrawnForStaking(address indexed caller, uint256 assets);
     event DepositedFromStaking(address indexed caller, uint256 amount);
+
+	/// @custom:oz-upgrades-unsafe-allow constructor
+	constructor() {
+		// The constructor is executed only when creating implementation contract
+		// so prevent it's reinitialization
+		_disableInitializers();
+	}
+
 
     function withdrawForStaking(uint256 amount) external onlyOwner {
         stakingTotalAssets += amount;
@@ -24,15 +32,26 @@ contract ERC4626Mock is Ownable, ERC4626 {
         IERC20(asset()).safeTransferFrom(msg.sender, address(this), amount);
     }
 
-    constructor(
+    function initialize(
         address underlying
-    )
-        ERC20("ggpVault", "ggGGP")
-        ERC4626(IERC20(underlying))
-        Ownable(msg.sender)
-    {
+    ) public initializer {
+        		__ERC4626Upgradeable_init(asset, "GoGoPool Liquid Staking Token", "ggAVAX");
+		__BaseUpgradeable_init(storageAddress);
+        // ERC20("ggpVault", "ggGGP");
+        // ERC4626(IERC20(underlying))
+        // Ownable(msg.sender)
+        version = 1;
         stakingTotalAssets = 0;
     }
+    // constructor(
+    //     address underlying
+    // )
+    //     ERC20("ggpVault", "ggGGP")
+    //     ERC4626(IERC20(underlying))
+    //     Ownable(msg.sender)
+    // {
+    //     stakingTotalAssets = 0;
+    // }
 
     function getUnderlyingBalance() public view returns (uint256) {
         return IERC20(asset()).balanceOf(address(this));
