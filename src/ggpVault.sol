ff58f9151a89d5c7785fc3d4750ef4dbae5234b9
bookland <rratcliffe57@gmail.com>
2024-02-04 09:55:05 +0900

    add mvp

--- src/ggpVault.sol
+++ src/ggpVault.sol
@@ -1,18 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
+import {ERC20Upgradeable, IERC20} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract ERC4626Mock is Ownable, ERC4626, Initializable, BaseUpgradeable {
+import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+
+interface Staking {
+	function stakeGGPOnBehalfOf(address stakerAddr, uint256 amount) external;
+}
+interface IStorageContract {
+    function getAddress(bytes32 _id) external view returns (address);
+}
+
+contract GGPVault is Initializable, Ownable2StepUpgradeable, ERC4626Upgradeable  {
+    IStorageContract public constant ggpStorage = IStorageContract(0x1cEa17F9dE4De28FeB6A102988E12D4B90DfF1a9);
+
     using SafeERC20 for IERC20;
     uint256 public stakingTotalAssets;
     event WithdrawnForStaking(address indexed caller, uint256 assets);
     event DepositedFromStaking(address indexed caller, uint256 amount);
 
-	/// @custom:oz-upgrades-unsafe-allow constructor
 	constructor() {
 		// The constructor is executed only when creating implementation contract
 		// so prevent it's reinitialization
@@ -20,10 +30,17 @@ contract ERC4626Mock is Ownable, ERC4626, Initializable, BaseUpgradeable {
 	}
 
 
-    function withdrawForStaking(uint256 amount) external onlyOwner {
-        stakingTotalAssets += amount;
-        emit WithdrawnForStaking(msg.sender, amount);
-        IERC20(asset()).safeTransfer(msg.sender, amount);
+    // function withdrawForStaking(uint256 amount) external onlyOwner {
+    //     stakingTotalAssets += amount;
+    //     emit WithdrawnForStaking(msg.sender, amount);
+    //     IERC20(asset()).safeTransfer(msg.sender, amount);
+    // }
+
+   function stakeOnValidator(uint256 amount, address nodeOp) external onlyOwner {
+    stakingTotalAssets += amount;
+    Staking stakingContract = Staking(getStakingContractAddress());
+    stakingContract.stakeGGPOnBehalfOf(nodeOp, amount);
+    emit WithdrawnForStaking(nodeOp, amount);
     }
 
     function depositFromStaking(uint256 amount) public {
@@ -32,26 +49,21 @@ contract ERC4626Mock is Ownable, ERC4626, Initializable, BaseUpgradeable {
         IERC20(asset()).safeTransferFrom(msg.sender, address(this), amount);
     }
 
+    function getStakingContractAddress() public view returns (address) {
+        bytes32 args = keccak256(abi.encodePacked("contract.address", 'staking'));
+        return ggpStorage.getAddress(args);
+    }
+
     function initialize(
         address underlying
     ) public initializer {
-        		__ERC4626Upgradeable_init(asset, "GoGoPool Liquid Staking Token", "ggAVAX");
-		__BaseUpgradeable_init(storageAddress);
-        // ERC20("ggpVault", "ggGGP");
-        // ERC4626(IERC20(underlying))
-        // Ownable(msg.sender)
-        version = 1;
+        __ERC4626_init(IERC20(underlying));
+        // __ERC4626_init_unchained(IERC20(underlying));
+        // which of the above should this be???? they use the unchained in their mock for some reason ?? https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/master/contracts/mocks/token/GGPVaultUpgradeable.sol
+        __ERC20_init("ggpVault", "ggGGP");
         stakingTotalAssets = 0;
     }
-    // constructor(
-    //     address underlying
-    // )
-    //     ERC20("ggpVault", "ggGGP")
-    //     ERC4626(IERC20(underlying))
-    //     Ownable(msg.sender)
-    // {
-    //     stakingTotalAssets = 0;
-    // }
+ 
 
     function getUnderlyingBalance() public view returns (uint256) {
         return IERC20(asset()).balanceOf(address(this));
@@ -61,3 +73,4 @@ contract ERC4626Mock is Ownable, ERC4626, Initializable, BaseUpgradeable {
         return stakingTotalAssets + getUnderlyingBalance();
     }
 }
+
--- test/ERC4626StdTest.test.sol
+++ test/ERC4626StdTest.test.sol
@@ -4,12 +4,13 @@ pragma solidity >=0.8.0 <0.9.0;
 import "erc4626-tests/ERC4626.test.sol";
 
 import {ERC20Mock} from "../src/ERC20Mock.sol";
-import {ERC4626Mock} from "../src/ggpVault.sol";
+import {GGPVault} from "../src/ggpVault.sol";
 
 contract ERC4626StdTest is ERC4626Test {
     function setUp() public override {
         _underlying_ = address(new ERC20Mock());
-        _vault_ = address(new ERC4626Mock(_underlying_));
+        _vault_ = address(new GGPVault());
+        GGPVault(_vault_).initialize(_underlying_);
         _delta_ = 0;
         _vaultMayBeEmpty = false;
         _unlimitedAmount = false;
